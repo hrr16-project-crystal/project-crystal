8e15b11337aeaa21815993ed9423ae939fbdaae0
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-16 18:51:18 -0500

    Cleanup and modularize code
    Fixed linting errors, reorganized components, modularized css.

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {Router, Route, browserHistory} from 'react-router';
-import App from './components/app';
-import './main.css';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+import App from './components/App';
+import LandingPage from './components/LandingPage';
+import './index.css';
 
-// provider component wraps our entire application so that it can be
-// connected to our redux store, and we can access redux store in our components
 render((
   <Router history={browserHistory}>
     <Route component={App}>
-      <Route path="/" component={App}>
-      </Route>
+      <Route path="/" component={LandingPage} />
     </Route>
   </Router>
 ), document.getElementById('app'));
