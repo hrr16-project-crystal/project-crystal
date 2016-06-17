a5ef88ecbdb0fecccf11c4534f82cc25333ccf61
Jason Bacchetta <jabacchetta@users.noreply.github.com>
2016-06-16 20:46:49 -0500

    Cleanup and modularize code (#22)
    * Initialize directory structure
    
    Setup the boilerplate file structure and code.
    
    * Complete landing page
    
    Finished the landing page and added missing modules.
    
    * Cleanup and modularize code
    
    Fixed linting errors, reorganized components, modularized css.
    
    * Cleanup and modularize code
    
    Fixed linting errors, reorganized components, modularized css.
    
    * Add webpack modules
    
    Installed plugins for additional webpack functionality.

--- client/src/components/app/index.js
+++ client/src/components/app/index.js
@@ -1,45 +1,19 @@
-import React, {PropTypes} from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
   render() {
     return (
-      <div className="hero">
-        <div className="hero__overlay">
-          <nav>
-            <a className="logo">Sparkq<span className="logo__dot">.</span></a>
-            <ul className="menu">
-              <li className="menu__item"><a className="menu__link" href="">Services</a>
-              </li>
-              <li className="menu__item"><a className="menu__link"
-                                            href="">About</a></li>
-              <li className="menu__item"><a className="menu__link" href="">Contact</a>
-              </li>
-              <li className="menu__item"><a className="menu__link"
-                                            href="">Login</a></li>
-            </ul>
-          </nav>
-          <div className="hero__promo">
-            <h4 className="hero__promo__pretitle">Reimagining What It Means</h4>
-            <h1 className="hero__promo__title">To Be In Love</h1>
-            <p className="hero__promo__subtitle">Relationship suggestions. Date
-              reminders.
-              Honey do lists. Appreciation notes. Passion building. Personal
-              Growth
-              .</p>
-            <a href=""
-               className="hero__promo__btn hero__promo__btn--primary"> <span
-              className="">Learn
-    More</span></a>
-            <a href="" className="hero__promo__btn hero__promo__btn--secondary">
-              <span className="">Get The App</span></a>
-          </div>
-        </div>
+      <div>
+        {this.props.children}
       </div>
     );
   }
 }
 
-App.propTypes = {};
+App.propTypes = {
+  children: React.PropTypes.object.isRequired,
+};
+
 App.defaultProps = {};
 
 export default App;
