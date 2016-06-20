a3fde98c01f1c8c20858014bef260b34f4fd2a12
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-20 00:22:08 -0500

    Prepare for Redux refactor
    Get quiz component ready for being refactored to work with Redux.

--- client/src/Quiz/index.js
+++ client/src/Quiz/index.js
@@ -1,56 +1,57 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './index.css';
 
 class Quiz extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       //get from db later
       questions: [
-        {id: 1, question: "Did you give your partner a compliment today?"},
-        {id: 2, question: "Did you give your partner a hug today?"}, 
-        {id: 3, question: "Did you help your partner in any way today?"}
+        { id: 1, question: "Did you give your partner a compliment today?" },
+        { id: 2, question: "Did you give your partner a hug today?" },
+        { id: 3, question: "Did you help your partner in any way today?" }
       ]
     };
   }
 
-  componentWillMount(){
+  componentWillMount() {
     this.getQuestions();
   }
 
   //replace URL once available
-  getQuestions(){
+  getQuestions() {
     $.ajax({
-     type: 'GET',
-     contentType: 'application/json',
-     url: '/api/questions',
-     success: questions => this.setState({ questions: questions }),
-   });
+      type: 'GET',
+      contentType: 'application/json',
+      url: '/api/questions',
+      success: questions => this.setState({ questions: questions }),
+    });
   }
 
-  postResponse(event){
+  postResponse(event) {
     event.preventDefault();
     $.ajax({
-       type: 'POST',
-       contentType: 'application/json',
-       url: '/api/user/questions/answered',
-       data: JSON.stringify(data),
-       success: data => {
-         console.log('ajax called, postResponse sent');
-       },
-       error: function (data) {
-         console.error('ajax called, postResponse failed');
-       },
+      type: 'POST',
+      contentType: 'application/json',
+      url: '/api/user/questions/answered',
+      data: JSON.stringify(data),
+      success: data => {
+        console.log('ajax called, postResponse sent');
+      },
+      error: function (data) {
+        console.error('ajax called, postResponse failed');
+      },
     });
   }
 
   render() {
+    //need to pass id of question answered to post request
     return (
       <div className="quiz-box">
-        //need to pass id of question answered to post request
         <p>{this.state.questions.pop()}</p>
-        <input type="submit" value="Yes" onClick={this.postResponse}>
-        <input type="submit" value="No" onClick={this.postResponse}>
+        <input type="submit" value="Yes" onClick={this.postResponse}/>
+        <input type="submit" value="No" onClick={this.postResponse}/>
       </div>
     );
   }
@@ -59,4 +60,8 @@ class Quiz extends Component {
 Quiz.PropTypes = {};
 Quiz.defaultProps = {};
 
-export default Quiz;
+function mapStateToProps() {
+
+}
+
+export default connect(mapStateToProps)(Quiz);
