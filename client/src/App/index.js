b84192bd55aef16a59604576d8eed5d415fe5aad
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-20 18:27:45 -0500

    Cleanup app
    Removed old code from App.

--- client/src/App/index.js
+++ client/src/App/index.js
@@ -1,27 +1,4 @@
-// import React, { Component } from 'react';
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         {this.props.children}
-//         Hello
-//       </div>
-//     );
-//   }
-// }
-
-// App.propTypes = {
-//   children: React.PropTypes.object.isRequired,
-// };
-
-// App.defaultProps = {};
-
-// export default App;
-
-
 import React, { Component } from 'react';
-// import Header from './Header';
 
 export default class App extends Component {
   render() {
