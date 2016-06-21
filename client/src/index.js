087c94cb60190007b39fa5026b395ec40da3a043
Pallandor <roger.sejas@gmail.com>
2016-06-22 09:21:29 +0930

    Resolve conflict on merge

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import { render } from 'react-dom';
-<<<<<<< HEAD
-//browserHistory keeps track of current URL and knows how to move to a new URL
-import { Router, Route, browserHistory } from 'react-router';
-//need to import components index.js files to route to in Router
-  //dont need to specify specific file name if using index.js convention
-  //for all components
-import App from './components/App';
-import LandingPage from './components/LandingPage';
-// require ('./index.css');
-=======
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
@@ -27,7 +17,6 @@ import reducers from './rootReducer';
 import { AUTH_USER } from './constants/types';
 
 import './index.css';
->>>>>>> 96a1e9a3b04c1bb70f2f9297ce7e330f6798df27
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 const store = createStoreWithMiddleware(reducers);
