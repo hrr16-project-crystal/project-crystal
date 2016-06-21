234f7abd4ce7f86255f229dc36f0f24854af108b
cruz02148 <cruz02148@gmail.com>
2016-06-21 17:27:36 -0400

    Fixed Quiz component to allow the form to submit a post request to the server and the values entered by the user will stick

--- client/src/Quiz/QuizActions.js
+++ client/src/Quiz/QuizActions.js
@@ -3,22 +3,6 @@ import { MARK_ANSWERED, FETCH_QUESTIONS } from '../constants/types';
 import { browserHistory } from 'react-router';
 const apiUrl = 'http://localhost:3000';
 
-// replace URL once available
-// export function getQuestions() {
-//   return (dispatch) => {
-//     axios.get(`${apiUrl}/questions`)
-//       .then(response => {
-//         console.log(response);
-//         dispatch({
-//           type: FETCH_QUESTIONS,
-//           payload: response.data.answers,
-//           // contentType: 'application/json',
-//           // success: questions => this.setState({ questions: questions })
-//         });
-//       });
-//   };
-// }
-
 export const getQuestions = () => {
   return dispatch => {
     axios.get(`${apiUrl}/questions`)
@@ -31,11 +15,11 @@ export const getQuestions = () => {
   };
 };
 
-export function postResponse({ hug, compliment, kiss }) {
+export const postResponse = ({ hug, kiss }) => {
   return (dispatch) => {
-    // data: JSON.stringify(data)
-    axios.post(`${apiUrl}/questions/`, { hug, compliment, kiss })
+    axios.post(`${apiUrl}/questions/answered`, { hug, kiss })
       .then(response => {
+        console.log(response);
         dispatch({ type: MARK_ANSWERED });
         browserHistory.push('/dashboard');
       })
@@ -43,4 +27,4 @@ export function postResponse({ hug, compliment, kiss }) {
         console.log(response);
       });
   };
-}
+};
