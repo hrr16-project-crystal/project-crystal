39a8779eb0ea1a9e9c10c1719e6f4968b9778a65
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-22 23:14:36 -0500

    Refactor directory structure
    Cleaned up the directory structure and file names in an effort to keep
    some consistency.

--- client/src/components/Authentication/Signout.js
+++ client/src/components/Authentication/Signout.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from './authAction';
+<<<<<<< eee4d10ac0295d9dcd949d903cc535630d975626:client/src/Components/Authentication/Signout.js
 import Header from '../App/Header';
+=======
+import Header from '../App/header';
+>>>>>>> Refactor directory structure:client/src/Components/Authentication/Signout.js
 
 class Signout extends Component {
   componentWillMount() {
