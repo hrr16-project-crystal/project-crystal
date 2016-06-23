b4d9e66ef48de16ecd48737dde6faab6cdae0e77
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-23 17:40:06 -0500

    Refactor directory
    Refactored files and directories.

--- client/src/Components/Dashboard/Dashboard.js
+++ client/src/Components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+<<<<<<< d4356f4a5f8b7328365717c84cc3175da9b977b5
 <<<<<<< eee4d10ac0295d9dcd949d903cc535630d975626:client/src/Components/Dashboard/Dashboard.js
 import Header from '../App/Header';
 import Meter from '../Meter/Meter';
@@ -6,6 +7,10 @@ import Meter from '../Meter/Meter';
 import Header from '../App/header';
 import Meter from '../Meter/meter';
 >>>>>>> Refactor directory structure:client/src/Components/Dashboard/Dashboard.js
+=======
+import Header from '../App/Header';
+import Meter from '../Meter/Meter';
+>>>>>>> Refactor directory
 
 // export default class Dashboard extends Component {
 export default () =>
--- server/db/index.js
+++ server/db/index.js
@@ -33,11 +33,10 @@ const options = {
   // database: 'blossomly',
   // user: 'postgres',
 const config = {
-  host: 'ec2-50-112-35-113.us-west-2.compute.amazonaws.com',
+  host: 'localhost',
   port: 5432,
   database: 'blossomly',
-  user: 'other_user',
-  password: 'hottub'
+  user: 'postgres',
 };
 
 // const config = {
