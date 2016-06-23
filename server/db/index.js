51c77f27b08e37bb58def9f50551b3581cdcc881
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-23 17:42:48 -0500

    Add config
    Added a new password config.

--- server/db/index.js
+++ server/db/index.js
@@ -33,10 +33,11 @@ const options = {
   // database: 'blossomly',
   // user: 'postgres',
 const config = {
-  host: 'localhost',
+  host: 'ec2-50-112-35-113.us-west-2.compute.amazonaws.com',
   port: 5432,
   database: 'blossomly',
-  user: 'postgres',
+  user: 'other_user',
+  password: 'hottub'
 };
 
 // const config = {
