019b6640c5895ec09cdcaf405559cf51ba50da28
Josh Summerhays <russiansummer@gmail.com>
2016-06-23 17:16:58 -0600

    (refactor) added more css to signup page

--- client/src/Components/Authentication/Signup.js
+++ client/src/Components/Authentication/Signup.js
@@ -32,45 +32,46 @@ class Signup extends Component {
     return (
       <div className="signup--box">
         <div className="signup__overlay">
-          <form className="container valign row" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-            <fieldset className="form-group col 4">
+          <form className="signup__form" onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+            <h6 className="signup--box__title">A better relationship is right around the corner</h6>
+            <fieldset className="form-group">
               <label>First Name:</label>
               <input className="form-control" {...firstName} />
               {firstName.touched && firstName.error && <div className="error">{firstName.error}</div>}
             </fieldset>
-            <fieldset className="form-group col 4">
+            <fieldset className="form-group">
               <label>Last Name:</label>
-              <input className="form-control col 4" {...lastName} />
+              <input className="form-control" {...lastName} />
               {lastName.touched && lastName.error && <div className="error">{lastName.error}</div>}
             </fieldset>
-            <fieldset className="form-group col 4">
+            <fieldset className="form-group">
               <label>Email:</label>
-              <input className="form-control col 4" {...email} />
+              <input className="form-control" {...email} />
               {email.touched && email.error && <div className="error">{email.error}</div>}
             </fieldset>
-            <fieldset className="form-group col 4">
+            <fieldset className="form-group">
               <label>Password:</label>
               <input type="password" className="form-control" {...password} />
               {password.touched && password.error && <div className="error">{password.error}</div>}
             </fieldset>
-            <fieldset className="form-group col 4">
+            <fieldset className="form-group">
               <label>Confirm Password:</label>
               <input type="password" className="form-control" {...passwordConfirm} />
                 {passwordConfirm.touched && passwordConfirm.error &&
                   <div className="error">{passwordConfirm.error}</div>}
             </fieldset>
-            <fieldset className="form-group col 4">
-              <label>Would you like to initiate a new couple?</label>
+            <fieldset className="form-group">
+              <label>Would you like to start a new couple?</label>
               <div>
                 <select className="signup-questions" {...couple}>
                   <option>Select an answer...</option>
-                  <option value="yes">Yes</option>
-                  <option value="no">No</option>
+                  <option value="yes">Yes - my partner has not signed up yet</option>
+                  <option value="no">No - connect with my partner</option>
                 </select>
               </div>
             </fieldset>
             {this.renderAlert()}
-            <button action="Submit" className="btn btn-primary">Signup</button>
+            <button action="Submit" className="btn btn-primary waves-effect waves-light">Signup</button>
           </form>
         </div>
       </div>
