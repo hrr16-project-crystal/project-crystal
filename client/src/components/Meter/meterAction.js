79cc2c50701976e604ce83cb246c0fc6b4ab547e
cruz02148 <cruz02148@gmail.com>
2016-06-23 01:46:40 -0400

    Continue refactor of back end and front end for MVP

--- client/src/components/Meter/meterAction.js
+++ client/src/components/Meter/meterAction.js
@@ -7,8 +7,6 @@ export const getHealth = (coupleID) => {
   return dispatch => {
     axios.get(`${apiUrl}/couples/`+coupleID)
       .then(response => {
-        console.log('METER ACTIONS');
-        console.log(response);
         dispatch({
           type: FETCH_HEALTH,
           payload: response.data,
