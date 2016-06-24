5c7e7967b81794536b70051c330d2e589e23007c
nrandall <nrandall82@gmail.com>
2016-06-24 08:19:17 -0700

    Add chat component files

--- server/server.js
+++ server/server.js
@@ -52,6 +52,15 @@ app.use('/api/v1', lovebucksAPIroutes);
 
 router(app);
 
+// Socket.io
+io.on('connection', function(socket) {
+  // once socket-client emits 'chat msg' event from client, chatHandler function will be invoked
+  socket.on('chat msg', function chatHandler(msg) {
+    // server emits 'chat msg' event back to every socket-client
+    io.emit('chat msg', msg);
+  });
+});
+
 // // *** error handlers *** //
 // catch 404 and forward to error handler
 
