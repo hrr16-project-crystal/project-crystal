ac5bc877c2777064c1e29c4573df9dd646639d6e
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-23 21:56:51 -0500

    Fix rebase errors
    Making changes to rebase structure.

--- client/src/components/Authentication/Signout.js
+++ client/src/components/Authentication/Signout.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from './authAction';
-<<<<<<< d4356f4a5f8b7328365717c84cc3175da9b977b5
-<<<<<<< eee4d10ac0295d9dcd949d903cc535630d975626:client/src/Components/Authentication/Signout.js
 import Header from '../App/Header';
-=======
-import Header from '../App/header';
->>>>>>> Refactor directory structure:client/src/Components/Authentication/Signout.js
-=======
-import Header from '../App/Header';
->>>>>>> Refactor directory
 
 class Signout extends Component {
   componentWillMount() {
--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,22 +1,6 @@
-<<<<<<< 27ba9b9cbcc75b2ea56f13c68a7010d0634b11e9
 import React from 'react';
 import Header from '../App/Header';
 import Meter from '../Meter/Meter';
-=======
-import React, { Component } from 'react';
-<<<<<<< d4356f4a5f8b7328365717c84cc3175da9b977b5
-<<<<<<< eee4d10ac0295d9dcd949d903cc535630d975626:client/src/Components/Dashboard/Dashboard.js
-import Header from '../App/Header';
-import Meter from '../Meter/Meter';
-=======
-import Header from '../App/header';
-import Meter from '../Meter/meter';
->>>>>>> Refactor directory structure:client/src/Components/Dashboard/Dashboard.js
-=======
-import Header from '../App/Header';
-import Meter from '../Meter/Meter';
->>>>>>> Refactor directory
->>>>>>> Refactor directory
 
 // export default class Dashboard extends Component {
 export default () =>
