12576ccc82892dfb29e23f650d3f169b4e68123d
Jason Bacchetta <jabacchetta@gmail.com>
2016-06-23 22:28:51 -0500

    Finish rebase changes
    Compelted rebase changes.

---
+++ client/src/components/Quiz/Question.js
@@ -0,0 +1,13 @@
+import React, { PropTypes } from 'react';
+
+const Question = (props) => {
+  return (
+    <div>
+      <p>{this.props.question}</p>
+    </div>
+  );
+};
+
+Question.PropTypes = {};
+
+export default Question;
