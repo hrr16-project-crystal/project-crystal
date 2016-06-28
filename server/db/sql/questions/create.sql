9d67c0b5e4330e2abbf3c4e35af2664a31e18e05
Pallandor <roger.sejas@gmail.com>
2016-06-29 03:21:55 +0930

    (Local) Mark prior to rebase

--- server/db/sql/questions/create.sql
+++ server/db/sql/questions/create.sql
@@ -2,26 +2,24 @@
     Creates Questions table
 */
 
-CREATE EXTENSION IF NOT EXISTS citext; 
+CREATE EXTENSION IF NOT EXISTS citext;
 
 CREATE TABLE Questions (
 question_id serial PRIMARY KEY,
-category_id integer,
+category citext NOT NULL,
 content citext NOT NULL,
 created_at timestamp NOT NULL DEFAULT now(),
-updated_at timestamp NOT NULL DEFAULT now(),
+updated_at timestamp NOT NULL DEFAULT now()
 );
 
--- CREATE FUNCTION update_updated_at_column() RETURNS trigger
---     LANGUAGE plpgsql
---     AS $$
---   BEGIN
---     NEW.updated_at = NOW();
---     RETURN NEW;
---   END;
--- $$;
+-- CREATE FUNCTION update_updated_at_column() RETURNS trigger AS $$
+-- BEGIN
+-- NEW.updated_at = NOW();
+-- RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
 
--- CREATE TRIGGER questions_updated_at_modtime 
---   BEFORE UPDATE ON Questions 
---   FOR EACH ROW 
---   EXECUTE PROCEDURE update_updated_at_column();
+-- CREATE TRIGGER questions_updated_at_trigger
+-- BEFORE UPDATE ON Questions
+-- FOR EACH ROW
+-- EXECUTE PROCEDURE update_updated_at_column();
