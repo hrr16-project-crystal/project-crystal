eb49512f785bd08edac61d0dac507180630f23b9
Jason Bacchetta <jabacchetta@users.noreply.github.com>
2016-07-06 23:14:29 -0500

    Add test coverage (#118)
    * Add Coveralls
    
    Added Coveralls integration with Travis-CI.
    
    * Fix Travi-CI
    
    An attempt to fix failed build.
    
    * Add Istanbul
    
    Added support for Instanbul with Travis_CI integration.
    
    * Add mock store
    
    Setup mock store for meter.
    
    * Add test note
    
    Added a quick note about future tests.
    
    * Modify Spark score
    
    Commented out my custom score positioning. Going back to using default
    position until we figure out a solution.

--- client/src/tests/meter/meterSpec.js
+++ client/src/tests/meter/meterSpec.js
@@ -2,7 +2,7 @@
 unit tests client - mocha, karma  **
 unit tests server - mocha  **
 integration tests - supertest  **
-code coverage - coveralls **
+code coverage - coveralls/istanbul **
 end to end tests - casper - maybe
 visual tests - phantomcss - maybe
 continuous integration - travis-ci  **
@@ -21,4 +21,40 @@ const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
 
 describe('Meter', () => {
+  let store;
+  beforeEach(() => {
+    store = mockStore({
+      auth: { 
+        user: { 
+          data: { 
+            couple_id: 1 
+          } 
+        } 
+      },
+      meter: {
+        health: {
+          data: {
+            score: 7,
+            respect_score: 1,
+            communication_score: 1,
+            intimacy_score: 2,
+            generosity_score: 5,
+            spontaneity_score: 6,
+          }
+        }
+      },
+      calendar: {},
+      form: {},
+      messages: {},
+      quiz: {},
+    });
+  });
+
+  // it('should render a TodoAdd component', () => {
+  //   const wrapper = mount(<Provider store={store}>
+  //     <TodoList /></Provider>);
+  //   expect(wrapper.find(TodoAdd)).to.have.length(0);
+  // });
+  
+  
 });
